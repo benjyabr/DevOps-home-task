ebab16149630b492633150a93d8909ff4b796c9f
Benjy Abraham <benjyabraham123456789@gmail.com>
2024-07-14 04:20:07 +0300

    realign outputs to output in select locations

--- invoke_lambdas/invoke_lambdas_script.sh
+++ invoke_lambdas/invoke_lambdas_script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #invoking both lambdas and printing out only the secret of the second lambda
-curl -X GET --url "$(cat /mnt/outputs/first_lambda_url.txt)" > /dev/null
-curl -X GET -H "Authorization: 946684800" --url "$(cat /mnt/outputs/second_lambda_url.txt)" | sed -n -e 's/^.*secret is: //p'
+curl -X GET --url "$(cat /mnt/output/first_lambda_url.txt)" > /dev/null
+curl -X GET -H "Authorization: 946684800" --url "$(cat /mnt/output/second_lambda_url.txt)" | sed -n -e 's/^.*secret is: //p'
